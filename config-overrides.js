ca71cc15cf800a7f788f875082790fdddeb652b6
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2022-11-26 08:04:15 -0600

    project setup

---
+++ config-overrides.js
@@ -0,0 +1,5 @@
+const paths = require('react-scripts/config/paths');
+const path = require('path');
+
+paths.appSrc = path.resolve(__dirname, 'app');
+paths.appIndexJs = path.resolve(__dirname, 'app/index.js');
