c37150c6dde2a4ee2570e272db54289c64e392cb
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2022-12-17 09:13:24 -0600

    added source randomization (WIP)

--- src/controllers/sourceRandomization.ts
+++ src/controllers/sourceRandomization.ts
@@ -1,95 +1,38 @@
-import express, { Request, Response  } from 'express';
-import { randomFill } from 'crypto';
-import seedrandom from 'seedrandom';
 import axios from 'axios';
 
+async function randomParagraph() {
+  const randomTextURL = `https://story-shack-cdn-v2.glitch.me/generators/random-paragraph-generator`
 
-const app = express();
-
-async function generateSeed(): Promise<string> {
-    const seedBuffer = Buffer.alloc(32);
-    randomFill(seedBuffer, (error, buf) => {
-    if (error) {
-        console.error(error);
+  try {
+    const response = await axios.get(randomTextURL)
+    if (response.status === 200) {
+      const name: string = response.data.data.name;
+      return name
     } else {
-        console.log(buf);
+      throw new Error(`API request failed with status code ${response.status}`)
     }
-    });
-
-    let seedString = seedBuffer.toString('hex');
-    seedString = seedString.replace(/[^a-zA-Z]/g, '');
-    return seedString;
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
-async function randomParagraph(seed: string): Promise<string> {
-    const rng = seedrandom(seed);
-    const randomTextURL = `https://api.deepai.org/api/text-generator`;
-
-    try {
-      // Make the API request
-      const response = await axios.post(randomTextURL,  {
-          'text': rng,
-      },
-      {headers: {'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}});
-
-      // Check the response status code
-      if (response.status === 200) {
-        // Return the text if the request was successful
-        console.log(response.data.text)
-        return response.data.text;
-      } else {
-        // Throw an error if the request was not successful
-        throw new Error(`API request failed with status code ${response.status}`);
-      }
-    } catch (error) {
-      // Catch and handle any errors that occurred during the request
-      console.error(error);
-      throw error;
-    }
-}
-
-async function randomList(seed: string, size: number): Promise<string[]> {
+async function randomList(): Promise<string[]> {
+  const size = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
   const list: string[] = [];
   for (let i = 0; i < size; i++) {
-    const paragraph = await randomParagraph(seed);
+    const paragraph = await randomParagraph();
     list.push(paragraph);
   }
   return list;
 }
 
-async function randomListItem(seed: string): Promise<string> {
-  const list = await randomList(seed, 10);
-  return list[Math.floor(Math.random() * list.length)];
-}
-
-app.post('/random', async (req: Request, res: Response) => {
-    const seed = req.body.seed;
-    if (typeof seed !== 'number') {
-      res.status(400).send({ error: 'Seed must be an integer' });
-      return;
-    }
-    try {
-      const item = await randomListItem(seed.toString());
-      res.send({ item });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: 'Something went wrong' });
-    }
-});
-  
 async function sourceInsert(str: string) {
-    const listItem = await randomListItem(str);
-    return str.replace(/<!--HUTAOWOA-->/g, function() { return listItem; });
+  const list = await randomList();
+  return str.replace(/<!--HUTAOWOA-->/g, `<span style="display: none;">${list.join('')}</span>`);
 }
 
 export async function paintSource(str: string) {
-    str = await sourceInsert(str);
-    return str;
+  str = await sourceInsert(str);
+  return str;
 }
-  
-  
-  
-  
-  
-
-
